50c5fd1fc350142dfbac9bd97ceae6124ffbb94b
johirul-islam <mdj601399@gmail.com>
2025-08-12 00:30:24 +0600

    add money section

--- js/login.js
+++ js/login.js
@@ -1,8 +1,18 @@
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('login button clicked');
+    
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    // console.log(phoneNumber);
+
+    const pinNumber =document.getElementById('pin-number').value;
+    console.log( phoneNumber, pinNumber);
+    if(phoneNumber==='54'&& pinNumber==='1234'){
+        console.log('you are loging');
+        window.location.href ='/home.html';
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
 })
 
