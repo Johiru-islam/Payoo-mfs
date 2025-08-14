7059af2f7bf31b2a45e25aceb359108496d23589
johirul-islam <mdj601399@gmail.com>
2025-08-14 08:21:27 +0600

    added the partiallay the add money event handeler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-console.log('home added')
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const addmoneyinput = document.getElementById('input-add-money').value ;
+    console.log(addmoneyinput);
+
+    const pinNumber = document.getElementById('input-pin-number').value ;
+    console.log(pinNumber);
+})
