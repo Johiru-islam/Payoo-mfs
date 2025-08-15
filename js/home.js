0bfb850872497b1008d31f7089ca49112da2b151
johirul-islam <mdj601399@gmail.com>
2025-08-15 06:54:22 +0600

    add money

--- js/home.js
+++ js/home.js
@@ -1,8 +1,27 @@
 document.getElementById('btn-add-money').addEventListener('click',function(event){
     event.preventDefault();
-    const addmoneyinput = document.getElementById('input-add-money').value ;
-    console.log(addmoneyinput);
+    const addmoneyInput = document.getElementById('input-add-money').value ;
+    console.log(addmoneyInput);
 
-    const pinNumber = document.getElementById('input-pin-number').value ;
-    console.log(pinNumber);
+    const pinNumberInput = document.getElementById('input-pin-number').value ;
+    console.log(pinNumberInput);
+
+    if(pinNumberInput ==='1234'){
+        console.log('adding money to your account')
+
+        const balance = document.getElementById('account-balance').innerText;
+        console.log( typeof balance);
+
+        const addMoneyNumber = parseFloat(addmoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('fail to add money! please try again')
+    }
+
 })
